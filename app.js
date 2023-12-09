19ab2307e7d384341d17bc6dec02062631243bb7
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 03:47:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// Question 01
+var num1=1 , num2=2 , num3=3; 
+
+alert(num1)
+
+// Question 02Variables must begin with a,letter,$ and _.For example $name, _name or name
+
+
+// Question 03
+
+document.write("<h1>Rules for naming JS variables</h1>")
+
+document.write("Variable names can only contain, Number,$and_.For example $my_1stVariable")
+
+document.write("<br>"+"Variables must begin with a,letter,$ and _.For example $name, _name or name")
+
+document.write("<br>"+"Variable names are case Sensitive")
+
+document.write("<br>"+"Variable names should not be JS Keyword")
